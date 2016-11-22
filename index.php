07ed95ba718541e3c7fefd1bb216e2652f0fb834
fabiozaffani@gmail.com <fabiozaffani@gmail.com>
2016-11-22 11:13:39 -0200

    Atualizado site para remover o antigo e deixar uma página inicial básica introdutória

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-
-define('WP_USE_THEMES', true);
-
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="pt-BR">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="pt-BR">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html lang="pt-BR">
+<!--<![endif]-->
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" />
+    <title>Amazon Brazil Nuts | Amazon Brazil Nuts</title>
+    <link type="text/css" href="style.css" rel="stylesheet"></link>
+  </head>
+  <body>
+    <img src="/logo.jpg" />
+    <h1>Estamos alterando nosso site para melhor atendê-lo</h1>
+    <p>Para entrar em contato conosco envie um e-mail para
+      <a href="mailto:sac@amazonbrazilnuts.com.br?Subject=Contato%20Amazon%20Brazil%20Nuts">sac@amazonbrazilnuts.com.br</a> ou
+      <a href="mailto:comercial@amazonbrazilnuts.com.br?Subject=Contato%20Amazon%20Brazil%20Nuts">comercial@amazonbrazilnuts.com.br</a>
+    </p>
+    <p>Ou ligue para nós no telefone 55 (11) 3023-6813</p>
+  </body>
+</html>
